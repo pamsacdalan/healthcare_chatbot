6dd2baff22b70684283a6909277b23968217b7d3
John Nicholas Dato <jdato@77global.biz>
2023-11-08 16:19:15 +0800

    added login,logout and signup function

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,17 @@
+from xml.etree.ElementInclude import include
 from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.login, name="login"),
-    path('login/', views.login, name="login"),
+    # path('', views.login, name="login"),
+    path('', views.user_login, name='login'),
+    # path('login/', views.login, name="login"),
     path('home/', views.home, name="home"),
-    
+
+    # user login/logout
+    # path('login/', views.user_login, name='login'),
+    path('signup/', views.user_signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
+
     
 ]
