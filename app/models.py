ea1770d735f9fc8243eb234102b8ba55fa1c4f5a
John Nicholas Dato <jdato@77global.biz>
2023-11-22 14:22:10 +0800

    changed DB and added a new model for dentists

--- app/models.py
+++ app/models.py
@@ -3,4 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Dentist(models.Model):
-    dentist_name = models.CharField(max_length=1000)
+    dentist_name = models.CharField(max_length=10000)
+    clinic_name = models.CharField(max_length=10000)
+    clinic_address = models.CharField(max_length=10000)
+    city_town = models.CharField(max_length=10000)
+    province = models.CharField(max_length=1000)
+    region = models.CharField(max_length=1000)
+    zipcode = models.IntegerField()
+    contact_number = models.CharField(max_length=10000)
+    clinic_schedule = models.CharField(max_length=10000) 
--- chatbot/settings.py
+++ chatbot/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'dentalcare',
+    'NAME': 'healthcare_dental',
     'USER': 'johnnicholasmdato',
     'PASSWORD': 'JBmMq1Dsd3fn',
     'HOST': 'ep-still-pine-74876349.ap-southeast-1.aws.neon.tech',
